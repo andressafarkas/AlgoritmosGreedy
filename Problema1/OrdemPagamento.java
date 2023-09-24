7c6e731e4113011fe1aa839d1e45b5387fae7ca0
Andressa Farkas <andressafarkas08@gmail.com>
2023-09-24 13:52:56 -0300

    Corrigido problema na questao 1

--- Problema1/OrdemPagamento.java
+++ Problema1/OrdemPagamento.java
@@ -13,19 +13,17 @@ public class OrdemPagamento {
 
         // Criar uma lista de empréstimos com seus índices e taxas de juros correspondentes
         for (int i = 0; i < n; i++) {
-            emprestimos.add(new Emprestimo(i + 1, taxasJuros.get(i)));
+            emprestimos.add(new Emprestimo(i, taxasJuros.get(i)));
         }
 
         // Ordenar os empréstimos com base nas taxas de juros em ordem crescente
-        Collections.sort(emprestimos, Comparator.comparingDouble(emprestimo -> emprestimo.taxaDeJuros));
+        Collections.sort(emprestimos, Collections.reverseOrder(Comparator.comparingDouble(emprestimo -> emprestimo.taxaDeJuros)));
 
         List<Integer> ordemPagamento = new ArrayList<>();
-        //double custoTotal = 0;
 
         // Pagar os empréstimos em ordem crescente de taxa de juros
         for (Emprestimo emprestimo : emprestimos) {
             ordemPagamento.add(emprestimo.id);
-            //custoTotal += Math.pow(emprestimo.taxaDeJuros, ordemPagamento.size() - 1) * 1000;
         }
 
         return ordemPagamento;
--- main.java
+++ main.java
@@ -1,15 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import Problema1.OrdemPagamento;
 public class main {
         public static void main(String[] args) {
 
         List<Double> taxasJuros = new ArrayList<>();
-        taxasJuros.add(1.1);
-        taxasJuros.add(1.05);
-        taxasJuros.add(1.2);
-       
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos empréstimos foram contratados? ");
+        int num_emprestimos = scanner.nextInt();
+
+        for (int i = 0; i < num_emprestimos; i++) {
+            System.out.println("Qual a taxa de juros do empréstimo " + i + " ? ");
+            taxasJuros.add(scanner.nextDouble());
+        }
+
+        System.out.println(taxasJuros);
+
+ 
         System.out.println("Ordem de pagamento dos empréstimos");
         for (Integer id : OrdemPagamento.menorCustoTotal(taxasJuros)) {
             System.out.println("Empréstimo: " + id);
